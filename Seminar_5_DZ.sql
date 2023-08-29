7fd3bf419f1691fe011e4b92c8a051972ee27e8a
asrbacr <gek2372@ya.ru>
2023-08-29 12:56:28 +0300

    Create Seminar_5_DZ.sql
    делаю ДЗ

---
+++ Seminar_5_DZ.sql
@@ -0,0 +1,19 @@
+USE semimar_4;
+
+SELECT * FROM profiles;
+SELECT * FROM users;
+SELECT * FROM messages;
+
+-- Создайте представление, в которое попадет информация о пользователях (имя, фамилия, город и пол), которые не старше 20 лет.
+SELECT lastname, firstname, hometown, gender FROM profiles
+JOIN users ON profiles.user_id = users.id
+WHERE 20 >= TIMESTAMPDIFF(YEAR, birthday, CURDATE());
+
+/* Найдите кол-во, отправленных сообщений каждым пользователем и выведите ранжированный список пользователей, 
+указав имя и фамилию пользователя, количество отправленных сообщений и 
+место в рейтинге (первое место у пользователя с максимальным количеством сообщений). 
+(используйте DENSE_RANK) */
+SELECT lastname, firstname, DENSE_RANK() OVER() 
+FROM users
+JOIN messages ON messages.from_user_id = users.id
+;
